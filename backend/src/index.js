ab7e613d950f4fe20d4e7a91c4134b2efcf1b0dc
Sean McNally <smcnally@cliquestudios.com>
2018-11-28 10:50:56 -0600

    having fun

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "variables.env" });
+const jwt = require("jsonwebtoken");
 const createServer = require("./createServer");
 const db = require("./db");
 
@@ -8,6 +9,16 @@ const server = createServer();
 // Use express middlware to handle cookies (JWT)
 server.express.use(cookieParser());
 // TODO Use express middlware to populate current user
+// decode JWT to get user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the user id on the reqests for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
